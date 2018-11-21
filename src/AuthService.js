0835ea06afe7d8935f54ce741ca7c18cafc4261e
emmaTu <emma.n.tuovinen@gmail.com>
2018-11-21 17:13:12 +0200

    auth0 preliminary configuration added

---
+++ src/AuthService.js
@@ -0,0 +1,42 @@
+import auth0 from 'auth0-js';
+import { AUTH_CONFIG } from './Auth0Config';
+
+export default class AuthService {
+  auth0 = new auth0.WebAuth({
+    domain: AUTH_CONFIG.domain,
+    clientID: AUTH_CONFIG.clientID,
+    redirectUri: AUTH_CONFIG.redirectUri,
+    audience: AUTH_CONFIG.audience,
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  login() {
+    console.log("morjes taas");
+    this.auth0.authorize();
+  }
+
+  handleAuthentication(history) {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.push("/");
+      } else if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  setSession(authResult) {
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    sessionStorage.setItem('access_token', authResult.accessToken);
+    sessionStorage.setItem('id_token', authResult.idToken);
+    sessionStorage.setItem('expires_at', expiresAt);
+  }
+
+  isAuthenticated() {
+    let expiresAt = JSON.parse(sessionStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+}
