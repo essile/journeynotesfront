8aa13b1086f11a562bc7c3621bd4061bf562faa3
emmaTu <emma.n.tuovinen@gmail.com>
2018-11-29 19:52:35 +0200

    localhost takas

--- src/Auth0Config.js
+++ src/Auth0Config.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'journeynotes.eu.auth0.com',
     clientID: 'ayBc8GHf6927aoLt71MDPrmuTyEkDAsI',
-    redirectUri: 'https://journeynotes.netlify.com/',
+    redirectUri: 'http://localhost:3000/startSession',
     audience: 'https://journeynotes.world'
 }
