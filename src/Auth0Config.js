81ec0cf4ae270059ad71a311c2e23d7ef71c8d84
emmaTu <emma.n.tuovinen@gmail.com>
2018-11-29 19:43:28 +0200

    redirecturl muutettu

--- src/Auth0Config.js
+++ src/Auth0Config.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'journeynotes.eu.auth0.com',
     clientID: 'ayBc8GHf6927aoLt71MDPrmuTyEkDAsI',
-    redirectUri: 'http://localhost:3000/startSession',
+    redirectUri: 'https://journeynotes.netlify.com/',
     audience: 'https://journeynotes.world'
 }
